f27b815aacc232fe47cb99e52b9e9bd7339286b2
jinyoung jang <jyjang@uengine.org>
2017-12-04 23:38:10 +0900

    modularize the metaworks4 components and register them to npm

---
+++ src/main/resources/static/src/components/index.js
@@ -0,0 +1,28 @@
+const Metaworks4 = {
+  install (Vue, opts = {}) {
+
+    const files = require.context('.', true, /\.vue$/);
+    const Components = {}
+    files.keys().forEach((key) => {
+      if (key === './index.js') {
+        return;
+      }
+      Components[key.replace(/(\.\/|\.vue)/g, '')] = files(key);
+    });
+    for (var key in Components) {
+      Vue.component(Components[key].name, Components[key]);
+
+      //console.log("installed : " + Components[key].name)
+    }
+
+    //윈도우 전역변수 등록 (다른 인스톨 플러그인에서 거진 하긴 해주지만 혹시 모르니...)
+    if (window && !window.Vue) {
+      window.Vue = Vue;
+    }
+  }
+}
+
+export default Metaworks4
+
+
+
